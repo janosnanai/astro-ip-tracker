369726c2e0a23eb63f32d1e5875614b79f0dabf8
Janos Nanai <janos.nanai@gmail.com>
2022-10-27 09:49:51 +0200

    revert to good old npm, prepare for netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
+  adapter: netlify()
 });
